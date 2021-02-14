837ad6f2e8aa6807adb787cc3bf3531c30e3df9e
Marcela Medina <medinam@aacog.local>
2021-02-14 17:40:34 -0600

    updating logic

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,5 +1,7 @@
+const API_KEY = "pk.eyJ1IjoibXRtZWRpbmEwMDE3IiwiYSI6ImNra3JrYzE5MjBkYTkycWxqdms4aTlmNTYifQ.LMyBlTCSu9zxE_pSUeCcDw";
+
 // Creating URL
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // Request to the query URL and creating features and functions
 d3.json(queryUrl, function(data) {
@@ -13,10 +15,24 @@ function earthquakeFeatures(earthquakeData){
 
   // Defining function and giving feature a popup
   function onEachFeaturePrep(feature, layer) {
-    layer.bindPopup("Magnitude: " + "<br>Location: " + feature.properties.mag + feature.properties.place);
+    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " +  feature.properties.place);
   }
 // Run the onEAchFeature and call geoJSON function
   var earthquakes = L.geoJSON(earthquakeData, {
+    pointToLayer: function(feature, latlng) {
+      return L.circle(latlng,
+        {radius: (feature.properties.mag) * 35000,
+
+        fillColor: getColor(feature.geometry.coordinates[2]),
+
+        color: "black",
+
+        stroke: true,
+
+        weight: 0.6
+
+        });
+    },
     onEachFeature: onEachFeaturePrep
   });
 
@@ -59,20 +75,19 @@ var baseMaps = {
   "Gray Map": graymap,
   "Satellite Map": satellitemap,
   "Outdoors": outdoors
-  
 };
 
 // Creating overlay
 var overlayMaps = {
-  Earthquakes: earthquales
+  Earthquakes: earthquakes
 };
 
-var myMap = L.map("map", {
+var myMap = L.map("mapid", {
   center: [
     37.09, -95.71
   ],
   zoom: 5,
-  layers: [streetmap, earthquakes]
+  layers: [outdoors, earthquakes]
 });
 
 L.control.layers(baseMaps, overlayMaps, {
@@ -80,6 +95,61 @@ L.control.layers(baseMaps, overlayMaps, {
 
 }).addTo(myMap);
 
+// an object legend
+var legend = L.control({
+  position: "bottomleft"
+});
+
+// details for the legend
+legend.onAdd = function() {
+  var div = L.DomUtil.create("div", "info legend");
+
+  var depth = [-10, 10, 30, 50, 70, 90];
+  var colors = [
+    "#98ee00",
+    "#d4ee00",
+    "#eecc00",
+    "#ee9c00",
+    "#ea822c",
+    "#ea2c2c"
+  ];
+
+// loop through our density intervals and generate a label with a colored square for each interval - https://leafletjs.com/examples/choropleth/
+for (var i = 0; i < depth.length; i++) {
+  div.innerHTML +=
+      '<i style="background:' + getColor(depth[i] + 1) + '"></i> ' +
+      depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' + '<br>' : '+');
 }
 
+return div;
+};
 
+legend.addTo(myMap);
+
+}
+
+// set different color from depth
+function getColor(depth) {
+  switch (true) {
+  case depth > 90:
+    return "#ea2c2c";
+  case depth > 70:
+    return "#ea822c";
+  case depth > 50:
+    return "#ee9c00";
+  case depth > 30:
+    return "#eecc00";
+  case depth > 10:
+    return "#d4ee00";
+  default:
+    return "#98ee00";
+  }
+}
+// set radiuss from magnitude
+  function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+
+  return magnitude * 4;
+}
